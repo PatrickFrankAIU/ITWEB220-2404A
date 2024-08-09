a0bf788a730153b59dd8102b39dedf11e597d02e
Ugochinyere Anazodo <ugo070@yahoo.com>
2024-08-09 09:27:39 -0400

    upload code

--- Ugo/main.js
+++ Ugo/main.js
@@ -1,3 +1,7 @@
-let var1 = prompt ("please a sentence!");
-let firstChar = var1[7];
-console.log("The first character of your string is:" + firstChar);
+let principal = prompt ("Enter the principal amount:");
+let interestRate =prompt ("Enter the interest rate (for 5% enter 5):");
+let timeYears = prompt ("For how many years is the investment");
+
+let rate = interestRate / 100;
+let interest = principal * rate * timeYears;
+console.log("The simple interest will be : $" + interest.toFixed(2));
