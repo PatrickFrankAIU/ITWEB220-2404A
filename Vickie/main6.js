cc69ce54076fe92c749727fc35fbb68886549f80
Vickie Miller <vickie.miller@paymentspring.com>
2024-08-13 21:26:20 -0500

    days of week

---
+++ Vickie/main6.js
@@ -0,0 +1,36 @@
+
+let currentDay = +prompt("Please enter a number 1 to 7:");
+
+switch(currentDay) {
+    case 1:
+       console.log("oh No it is Sunday, back to work tomorrow!");
+       break;
+ 
+    case 2:
+       console.log("I love Mondays!");
+       break;
+      
+   case 3:
+      console.log("Happy Tuesday!");
+      break;
+      
+   case 4:
+      console.log("Working hard Wednesday!");
+      break;
+      
+   case 5:
+      console.log("It is Thursday, one more day!");
+      break;
+      
+    case 6:
+      console.log("TGIF, it's Friday!");
+      break;
+    
+    case 7:
+      console.log("TGIF!!");
+      break;
+   
+    default:
+    console.error("invalid number");
+     break;
+}
