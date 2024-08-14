b491ca66073e9b2c860f4d61c88df3d939781b88
Vickie Miller <vickie.miller@paymentspring.com>
2024-08-13 22:27:42 -0500

    enter number

---
+++ Vickie/main7.js
@@ -0,0 +1,12 @@
+let num = +prompt("Please enter a number ");
+
+if (num < 0) {
+console.log("Number is Negative");
+}
+ else if (num > 0) {
+console.log("Number is Positive");
+}
+else {
+console.log("number is Zero");
+}
+
